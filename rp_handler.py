e929056c99f5ab0c3ac582e8dfb22731242afe1f
zapds <71247896+zapds@users.noreply.github.com>
2025-12-26 20:00:05 +0530

    remove unused files

--- rp_handler.py
+++ rp_handler.py
@@ -1,9 +1,9 @@
 from inference import Converter
 import subprocess
-from convex_helpers import download_video, upload_video_to_convex
 import runpod
 import logging
 import os
+import requests
 from convex import ConvexClient
 
 DEVICE = "cuda"  # or "cpu" if local testing without a GPU
@@ -20,6 +20,40 @@ CONVEX_URL = os.environ.get("CONVEX_URL")
 WORKER_SECRET = os.environ.get("WORKER_SECRET")
 
 client = ConvexClient(CONVEX_URL)
+
+
+
+def download_video(url, save_path):
+    """
+    Downloads a video from the specified URL and saves it to the given path.
+
+    Args:
+        url (str): The URL of the video to download.
+        save_path (str): The file path where the video will be saved.
+    """
+    response = requests.get(url, timeout=(5, 60), stream=True)
+    response.raise_for_status()
+
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+
+def upload_video_to_convex(url, file_path):
+    """
+    Uploads a video file to the specified Convex URL.
+
+    Args:
+        url (str): The Convex endpoint URL to upload the video to.
+        file_path (str): The path of the video file to upload.
+    
+    """
+    with open(file_path, 'rb') as file:
+        headers = {'Content-Type': 'video/webm'}
+        response = requests.post(url, headers=headers, data=file)
+        response.raise_for_status()
+    return response.json().get('storageId')
+
 
 def update_remove_bg_status(project_id: str, status: str, file_id: str | None = None, enabled: bool = True):
     import json
